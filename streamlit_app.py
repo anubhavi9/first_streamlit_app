d6de6b690b65a3da47635fbd379f23accf2ec40b
Anubhavi <117342465+anubhavi9@users.noreply.github.com>
2023-05-16 02:16:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit
-streamlit.title('The Title')
-streamlit.header('First Header')
-streamlit.text('This is the text')
+
+streamlit.title('My very own diner!')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
